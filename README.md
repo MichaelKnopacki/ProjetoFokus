b7efe37d52071c466384ed699fe57325415819af
Michael Knopacki <michaelknopacki@hotmail.com>
2024-11-09 09:02:58 -0300

    20241109

---
+++ README.md
@@ -0,0 +1,5 @@
+Curso: https://cursos.alura.com.br/course/javascript-manipulando-elementos-dom
+Figma : https://www.figma.com/design/dEaMv34Wd5G7TBMPo8fPlK/Projeto-Fokus?node-id=35-181&node-type=canvas 
+Git : 
+
+1. Iniciando o projeto Fokus
--- script.js
+++ script.js
@@ -3,15 +3,23 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 
+
+function clicado() {
+    alert('Funcionaaaaa');
+}
+
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    //html.setAttribute('data-contexto', 'foco')
+    alert('1');
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    //html.setAttribute('data-contexto', 'descanso-curto')
+    alert('2');
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    //html.setAttribute('data-contexto', 'descanso-longo')
+    alert('3');
 })
 
