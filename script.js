2b553c11860814db41e0cdaaf4fb22a71eafe68d
Michael Knopacki <michaelknopacki@hotmail.com>
2024-11-27 20:50:09 -0300

    20241127

--- script.js
+++ script.js
@@ -7,23 +7,42 @@ const image = document.querySelector('.app__image-figure')
 const frase = document.querySelector('.app__title')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musicaFoco = new Audio ('./sons/lun')
+
+/*
+Crie uma nova variável chamada ‘musicaFoco’ e utilize o objeto Audio para criar uma instância dele com o caminho do arquivo de música "Sons/Luna-Rise-Part-1.mp3";
+Defina o atributo loop do objeto Audio como true para tocar a música em loop;
+Adicione um evento de change ao input ‘musicaFocoInput’ usando addEventListener;
+Crie uma função anônima dentro do evento para verificar se a música está pausada utilizando a propriedade paused do objeto Audio:
+Se estiver pausada, utilize o método play para tocá-la;
+Caso contrário, utilize o método pause para pausar a música.
+*/
 
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto){
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute( 'src', `./imagens/${contexto}.png`)
+
+    botoes.forEach(function (contexto){
+       contexto.classList.remove('active')
+    })
 
     switch (contexto){
         case "foco":
